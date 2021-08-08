99b83b5dacfcf15667709d2dd393835da5f402f2
Neeraj319 <itskop520@gmail.comsource>
2021-08-08 14:30:10 +0545

    added selectionSort

---
+++ Sorting/selectionSort.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def sort(array: List) -> List:
+    for i in range(len(array) - 1):
+        for j in range(i, len(array)):
+            if array[i] > array[j]:
+                temp = array[j]
+                array[j] = array[i]
+                array[i] = temp
+    return array
+
+
+print(sort([4, 5, 2, 32423, 45]))
+print(sort([426, 32, 234]))
+print(sort([200, 2007, 124312, 412412]))
