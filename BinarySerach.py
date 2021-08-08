8c95e67d3597cb04dbdd428fb907be7e7872dfde
Neeraj319 <itskop520@gmail.comsource>
2021-08-08 11:22:45 +0545

    binary search added

---
+++ BinarySerach.py
@@ -0,0 +1,21 @@
+from typing import List, Union
+
+
+def search(array: List, element: int) -> Union[int, None]:
+    length = len(array)
+    high = length - 1
+    low = 0
+    while low <= high:
+        mid = high + low
+        if array[mid] == element:
+            return mid
+        if array[mid] > element:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+print(search([1, 2, 3, 4, 5, 6, 7], 5))
+print(search([10, 20, 30, 40], 5))
+print(search([22, 33, 45, 55, 654], 55))
