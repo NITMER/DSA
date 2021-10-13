0499e1ecfb89b0d93fe09736fb2429edec103249
NITMER <72238577+NITMER@users.noreply.github.com>
2021-10-13 21:05:32 +0545

    how to subtract two numbers using c
    ...

---
+++ substraction.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter two numbers: \n");
+  scanf("%d%d",&a,&b);
+  c=a-b;
+  printf("Difference between a and b is: %d",c);
+  retuen 0;
+}
